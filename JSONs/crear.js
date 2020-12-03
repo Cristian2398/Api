dc57c1ff77bebfe08782624d18f2e10af7478b79
efom444 <efortiz@unimayor.edu.co>
2020-12-03 18:07:46 -0500

    JSON crear listo

--- JSONs/crear.js
+++ JSONs/crear.js
@@ -5,3 +5,6 @@ use torneo_futbol;
 db.createCollection("torneo");
 db.createCollection("partidos");
 db.createCollection("equipos");
+db.createCollection("plantillas");
+db.createCollection("estadios");
+db.createCollection("jugadores");
