350feef88042aa994c2c5d9d940279e96f6ebabf
efom444 <efortiz@unimayor.edu.co>
2020-12-05 13:03:13 -0500

    plantillas intereface

---
+++ Api/Repositories/IPlantillasCollections.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Repositories
+{
+    interface IPlantillasCollections
+    {
+        Task InsertPlantillas(Plantillas plantillas);
+        Task UpdatePlantillas(Plantillas plantillas);
+        Task DeletePlantillas(string id);
+        Task<List<Plantillas>> GetAllPlantillas();
+        Task<Plantillas> GetPlantillasById(string id);
+    }
+}
---
+++ Api/Repositories/PlantillasCollections.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Repositories
+{
+    public class PlantillasCollections
+    {
+        public ObjectId Id { get; set; }
+        public string nombre_eq { get; set; }
+        public string edicion_torneo { get; set; }
+        public string fase { get; set; }
+        public string grupo { get; set; }
+        public Object jugadores { get; set; }
+    }
+}
